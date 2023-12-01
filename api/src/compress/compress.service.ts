77367569b0cd7eff8b3f9f7fb14f6e42aa5f32c1
kol4id <114818761+kol4id@users.noreply.github.com>
2023-12-01 04:09:54 +0300

    clean up unwanted comments

--- api/src/compress/compress.service.ts
+++ api/src/compress/compress.service.ts
@@ -4,10 +4,7 @@ import * as sharp from 'sharp'
 @Injectable()
 export class CompressService {
     async CompressImageFromBuffer(img: Buffer): Promise<Buffer>{
-        // let compressedImage: Buffer;
-        console.log(img)
         const compressedImage: Buffer = await sharp(img).resize(1280,720).webp({quality:70}).toBuffer()
-        console.log(compressedImage)
         return compressedImage;
     }
 }
