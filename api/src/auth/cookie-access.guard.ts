e3f9a53ac038735ea9387c3c91d1f54c2c2be68e
Kirill Nagulin <over90006at9@gmail.com>
2023-12-05 22:41:09 +0300

    created new guards for user auth, now fetching tokens from cookies

---
+++ api/src/auth/cookie-access.guard.ts
@@ -0,0 +1,38 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { MongoUserService } from "src/mongo/mongo-user.service";
+import { TokenService } from "src/token/token.service";
+
+
+@Injectable()
+export class CookieAccessGuard implements CanActivate{
+    constructor(
+        private tokenService: TokenService,
+        private mongoUserService: MongoUserService,
+    ){}
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const accessCookie = request.cookies['accessToken'];
+
+        if (!accessCookie){
+            throw new UnauthorizedException('AccessToken: accessToken cookie is empty');
+        }
+
+        let unsignCookie: any;
+        try {
+            unsignCookie = request.unsignCookie(accessCookie);
+        } catch (error: unknown){
+            throw new UnauthorizedException('AccessToken: accessToken cookie is not valid, try /refresh to refresh access token')
+        }
+        
+        const data = await this.tokenService.VerifyTokenAsync(unsignCookie.value, 'access');
+        const user = await this.mongoUserService.findById(data.id);
+
+        if (!user){
+            throw new UnauthorizedException('AccessToken: There is no such user') 
+        }
+
+        request.user = user
+
+        return true;
+    }
+}
---
+++ api/src/auth/cookie-refresh.guard.ts
@@ -0,0 +1,37 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { MongoUserService } from "src/mongo/mongo-user.service";
+import { TokenService } from "src/token/token.service";
+
+
+@Injectable()
+export class CookieRefreshGuard implements CanActivate{
+    constructor(
+        private tokenService: TokenService,
+        private mongoUserService: MongoUserService,
+    ){}
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const refreshCookie = request.cookies['refreshToken'];
+
+        if (!refreshCookie){
+            throw new UnauthorizedException('RefreshToken: refreshToken cookie is empty');
+        }
+
+        let unsignCookie: any;
+        try {
+            unsignCookie = request.unsignCookie(refreshCookie);
+        } catch (error: unknown) {
+            throw new UnauthorizedException('RefreshToken: refreshToken cookie is not valid');    
+        }
+        const data = await this.tokenService.VerifyTokenAsync(unsignCookie.value, 'refresh');
+        const user = await this.mongoUserService.findById(data.id);
+
+        if (!user){
+            throw new UnauthorizedException('RefreshToken: There is no such user') 
+        }
+
+        request.user = user
+        
+        return true;
+    }
+}
