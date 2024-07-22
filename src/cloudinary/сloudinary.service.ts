33ea5b9e94890b9e6345ac4ec749884d1a2aeffc
Kirill Nagulin <over90006at9@gmail.com>
2024-07-22 15:02:19 +0300

    change compress quality, cloudinary fix

--- src/cloudinary/сloudinary.service.ts
+++ src/cloudinary/сloudinary.service.ts
@@ -1,7 +1,5 @@
 import { Injectable, ServiceUnavailableException } from '@nestjs/common';
-import {UploadApiErrorResponse, UploadApiResponse, v2 as cloudinary} from 'cloudinary';
-import { error } from 'console';
-import { resolve } from 'path';
+import { v2 as cloudinary } from 'cloudinary';
 import { CompressService } from 'src/compress/compress.service';
 
 
@@ -12,17 +10,18 @@ export class CloudinaryService {
     async UploadImageByUrl (): Promise<void>{
         cloudinary.uploader.upload("https://avatars.githubusercontent.com/u/86874761?v=4",
         { resource_type: 'auto' }, 
-        function(error, result) {console.log(result); });
+        function(error, result) { });
     }
 
     //NOTE: compressing the image and changing format to .webp via compressService
     //Then sending it by a stream to cloudinary, handling errors
     //geting UploadApiReturn only if image successful sended
-    async UploadImageByFile (file: Buffer): Promise<string>{
+    async UploadImageByFile (file: Buffer, hash: string): Promise<string>{
         const compresedImg = await this.compressService.CompressImageFromBuffer(file);
         return new Promise((resolve) => {
             cloudinary.uploader.upload_stream({
-                resource_type: 'auto'
+                resource_type: 'auto',
+                public_id: hash
             }, (error, result) => {
                 if (error){
                     throw new ServiceUnavailableException('There some error while uploading your image');
@@ -32,17 +31,28 @@ export class CloudinaryService {
         })
     }
 
-    async UploadMultiple(files: Buffer[]): Promise<string[]>{
+    async UploadMultiple(files: Buffer[], hashes: string[]): Promise<string[]>{
         const urlsArr: string[] = [];
         await Promise.all(
-            files.map(async(file)=>{
-                const url = await this.UploadImageByFile(file);
+            files.map(async(file, index)=>{
+                const url = await this.UploadImageByFile(file, hashes[index]);
                 urlsArr.push(url);
         }));
         return urlsArr
     }
 
-    async FindImageById (id: string): Promise<void>{
-        console.log(await cloudinary.api.resources_by_asset_ids(id))
+    async FindImageById (id: string): Promise<any>{
+        return await cloudinary.api.resources_by_asset_ids(id)
     }
+
+    async findMedia(hash: string): Promise<any>{
+        let result = ''
+        try{
+            result = await cloudinary.api.resource(hash);
+        } finally {
+            return result
+        }
+    }
+
+
 }
